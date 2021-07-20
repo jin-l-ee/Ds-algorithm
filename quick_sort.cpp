3c5e888199572c2a78cabdc7e32224c17a7e9b8c
leeseockwoo <tjrdn110@naver.com>
2021-07-20 10:37:54 +0900

    sort

---
+++ quick_sort.cpp
@@ -0,0 +1,36 @@
+﻿#include<cstdio>
+#include <iostream>
+
+using namespace std;
+
+int a[1000];
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quickSort(int start, int end) {
+    if (start >= end) return;
+    int key = start, i = start + 1, j = end, temp;
+    while (i <= j) { // 엇갈릴 때까지 반복합니다. 
+        while (i <= end && a[i] <= a[key]) i++;
+        while (j > start && a[j] >= a[key]) j--;
+        if (i > j) swap(&a[key], &a[j]);
+        else swap(&a[i], &a[j]);
+    }
+    quickSort(start, j - 1);
+    quickSort(j + 1, end);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    quickSort(0, n - 1);
+    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+    system("pause");
+    return 0;
+}
---
+++ select_sort.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<cstdio>
+#define L 200005
+using namespace std;
+
+int arr[100];
+
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main() {
+
+	int n, min, index;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < n - 1; i++) {
+		int j = i;
+		while (j >= 0 && a[j] > a[j + 1]) {
+			swap(&a[j], &a[j + 1]);
+			j--;
+		}
+	}
+
+	for (int i= 0; i < n; i++) {
+		cout << arr[i];
+	}
+	cout << "end";
+
+
+	return 0;
+}
+
+
